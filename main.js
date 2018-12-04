8b4e39e271404f62893c781c90e011d2c1b8d2f2
unknown <ChristopherJohnson360@outlook.com>
2018-12-04 10:48:24 -0600

    edits to DOM selectin

---
+++ main.js
@@ -0,0 +1,26 @@
+// Use JavaScript to obtain a reference to the first section with the class of article_header and 
+// change its text with the textContent property to 
+// "Welcome to the {insert your name here} blog"
+const sectionEl = document.querySelector(".article__header");
+    console.log(sectionEl);
+    sectionEl.textContent = ("Welcome to the Acoustic blog");
+
+// Use JavaScript to obtain a reference to all article__header elements and change their 
+// classList property value to article__header important.
+
+const sectionEl = document.querySelectorAll(".article__header");
+let div class = ("article__header");
+div.classList.replace("article__header", "article__header important");
+
+// Obtain a reference the element with a class of dashed and remove it.
+const smallTextEl2 = document.querySelector(".dashed");
+    smallTextEl.classList.remove(".dashed");
+
+const div = document.createElement('div');
+div.className = article_Header
+
+// Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+const footerEl4 = document.querySelector(".article__footer");
+    footerEl.classList.add(".goldenrod");
+
+
